b738ef084c0387daf0b013a9c499b051be9a914c
Guillaume Guimond <guillaume.guimond@orange.com>
2019-03-27 09:54:13 +0100

    Flow control statements: for, if, else, switch and defer

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
     "fmt"
     "github.com/gguimond/gopractice/mylib"
+    "math"
+    "runtime"
+    "time"
 )
 
 func add(x, y int) int {
@@ -38,7 +41,24 @@ func needFloat(x float64) float64 {
     return x * 0.1
 }
 
+func sqrt(x float64) string {
+    if x < 0 {
+        return sqrt(-x) + "i"
+    }
+    return fmt.Sprint(math.Sqrt(x))
+}
+
+func pow(x, n, lim float64) float64 {
+    if v := math.Pow(x, n); v < lim {
+        return v
+    }
+    return lim
+}
+
 func main() {
+    defer fmt.Println("defer")
+    defer fmt.Println("will be called first")
+
     x := "short"
     fmt.Println("Hello, world.")
     fmt.Println(mylib.Reverse("toto"))
@@ -51,4 +71,38 @@ func main() {
     fmt.Println(needInt(Small))
     fmt.Println(needFloat(Small))
     fmt.Println(needFloat(Big))
+
+    sum := 0    
+    for i := 0; i < 10; i++ {
+        sum += i
+    }
+    fmt.Println(sum)
+
+    fmt.Println(sqrt(2), sqrt(-4))
+
+    fmt.Println(
+        pow(3, 2, 10),
+        pow(3, 3, 20),
+    )
+
+    switch os := runtime.GOOS; os {
+    case "darwin":
+        fmt.Println("OS X.")
+    case "linux":
+        fmt.Println("Linux.")
+    default:
+        // freebsd, openbsd,
+        // plan9, windows...
+        fmt.Printf("%s.\n", os)
+    }
+
+    t := time.Now()
+    switch {
+    case t.Hour() < 12:
+        fmt.Println("Good morning!")
+    case t.Hour() < 17:
+        fmt.Println("Good afternoon.")
+    default:
+        fmt.Println("Good evening.")
+    }
 }
