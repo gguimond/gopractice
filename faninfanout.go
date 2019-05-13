ce2d596bb3218c4f9d1014e5f605ca1aa9904e4e
Guillaume Guimond <guillaume.guimond@orange.com>
2019-05-13 10:07:51 +0200

    pipelines, fan in fan out

---
+++ faninfanout.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+    "fmt"
+    "sync"
+)
+
+func main() {
+    // Set up the pipeline.
+    c := gen(2, 3)
+    c1 := sq(c)
+    c2 := sq(c)
+
+    for n := range merge(c1, c2) {
+        fmt.Println(n)
+    }
+}
+
+func gen(nums ...int) <-chan int {
+    out := make(chan int)
+    go func() {
+        for _, n := range nums {
+            out <- n
+        }
+        close(out)
+    }()
+    return out
+}
+
+func sq(in <-chan int) <-chan int {
+    out := make(chan int)
+    go func() {
+        for n := range in {
+            out <- n * n
+        }
+        close(out)
+    }()
+    return out
+}
+
+func merge(cs ...<-chan int) <-chan int{
+    var wg sync.WaitGroup
+    out := make(chan int)
+
+    output := func(c <-chan int) {
+        for n := range c {
+            out <- n
+        }
+        wg.Done()
+    }
+
+    wg.Add(len(cs))
+
+    for _, c := range cs {
+        go output(c)
+    }
+
+    go func() {
+        wg.Wait()
+        close(out)
+    }()
+
+    return out
+}
