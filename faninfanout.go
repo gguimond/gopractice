4772fe99187bed8cc9cd61dfad1db849209a9b6e
Guillaume Guimond <guillaume.guimond@orange.com>
2019-05-13 17:52:45 +0200

    pipeline : explicit cancellation

--- faninfanout.go
+++ faninfanout.go
@@ -6,45 +6,64 @@ import (
 )
 
 func main() {
-    // Set up the pipeline.
-    c := gen(2, 3)
-    c1 := sq(c)
-    c2 := sq(c)
+    done := make(chan struct{})
+    defer close(done)
 
-    for n := range merge(c1, c2) {
+    // Set up the pipeline.
+    c := gen(done, 2, 3)
+    c1 := sq(done, c)
+    c2 := sq(done, c)
+
+    
+    for n := range merge(done, c1, c2) {
         fmt.Println(n)
     }
+
 }
 
-func gen(nums ...int) <-chan int {
-    out := make(chan int, len(nums))
-    for _, n := range nums {
-        out <- n
-    }
-    close(out)
-    return out
-}
-
-func sq(in <-chan int) <-chan int {
+func gen(done <-chan struct{}, nums ...int) <-chan int {
     out := make(chan int)
     go func() {
-        for n := range in {
-            out <- n * n
+        defer close(out)
+        for _, n := range nums {
+            select {
+                case out <- n:
+                case <- done:
+                    return
+            }
         }
-        close(out)
     }()
     return out
 }
 
-func merge(cs ...<-chan int) <-chan int{
+func sq(done <-chan struct{}, in <-chan int) <-chan int {
+    out := make(chan int)
+    go func() {
+        defer close(out)
+        for n := range in {
+            select {
+                case out <- n * n:
+                case <- done:
+                    return
+            }
+        }
+    }()
+    return out
+}
+
+func merge(done <-chan struct{}, cs ...<-chan int) <-chan int{
     var wg sync.WaitGroup
-    out := make(chan int, 1)
+    out := make(chan int)
 
     output := func(c <-chan int) {
+        defer wg.Done()
         for n := range c {
-            out <- n
+            select {
+                case out <- n:
+                case <-done:
+                    return
+            }
         }
-        wg.Done()
     }
 
     wg.Add(len(cs))
